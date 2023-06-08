419fd9d10d00da34e0267ba76804e12d144b3a32
Ridhanshu Jasrotia <jasrotia.ridhanshu.2017359@gmail.com>
2023-06-08 22:03:25 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
 import Home from "./components/pages/Home";
 import Home2 from "./components/pages/Home2";
 import Navbar from "./components/pages/Navbar";
@@ -7,14 +6,10 @@ import About from "./components/pages/About";
 import SearchJob from "./components/pages/SearchJob";
 import Feedback from "./components/pages/Feedback";
 import Footer from "./components/pages/Footer";
-import StudentProfile from "./components/profileModules/StudentProfile";
-import CompanyProfile from "./components/profileModules/CompanyProfile";
 import CheckLogin from "./components/pages/CheckLogin";
 import NotFound from "./components/pages/NotFound";
 import { useAuth0 } from "@auth0/auth0-react";
-
 import "./App.css";
-
 const App = () => {
   const { isAuthenticated } = useAuth0();
 
@@ -28,15 +23,12 @@ const App = () => {
             <Route path="/About" element={<About />} />
             <Route path="/SearchJob" element={<SearchJob />} />
             <Route path="/Feedback" element={<Feedback />} />
-            <Route path="/StudentProfile" element={<StudentProfile />} />
-            <Route path="/CompanyProfile" element={<CompanyProfile />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         ) : (
           <Routes>
             <Route path="/" element={<Home2 />} />
             <Route path="/About" element={<About />} />
-
             <Route path="/SearchJob" element={<CheckLogin />} />
             <Route path="/Feedback" element={<CheckLogin />} />
             <Route path="/StudentProfile" element={<CheckLogin />} />
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,6 +2,109 @@ import React from "react";
 import "../styles/Home.css";
 
 function Home() {
+  // Array of image URLs
+  const imageUrls = [
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230978/PlacementProject/image1_xess47.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230978/PlacementProject/image2_oj43jc.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230978/PlacementProject/image3_e9uu7h.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230978/PlacementProject/image4_s3jpfs.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230978/PlacementProject/image5_ygccjz.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230978/PlacementProject/image6_n2ddqc.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230979/PlacementProject/image7_wdfg52.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230979/PlacementProject/image8_f0licb.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230979/PlacementProject/image9_aqiuuv.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230979/PlacementProject/image10_evrojr.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230979/PlacementProject/image11_sugwkz.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230979/PlacementProject/image12_hnsbsg.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230980/PlacementProject/image13_dasfri.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230979/PlacementProject/image14_om2ovf.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230979/PlacementProject/image15_mkfzml.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230979/PlacementProject/image16_bocgyo.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230979/PlacementProject/image17_umzazm.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230980/PlacementProject/image18_rhxn3j.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230980/PlacementProject/image19_mfgbuk.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230980/PlacementProject/image20_frhdcf.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230980/PlacementProject/image21_e4ynlj.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230980/PlacementProject/image22_vrxstl.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230980/PlacementProject/image23_qg4dla.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230980/PlacementProject/image24_locahi.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230981/PlacementProject/image25_foxq8b.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230981/PlacementProject/image26_c5fr8g.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230968/PlacementProject/image27_frmgsf.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230968/PlacementProject/image28_qwchqi.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230968/PlacementProject/image29_kbcwwy.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230968/PlacementProject/image30_suuv9k.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230968/PlacementProject/image31_yuvolc.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230969/PlacementProject/image32_c8m9mv.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230969/PlacementProject/image33_bpdyja.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230968/PlacementProject/image34_in15bu.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230968/PlacementProject/image35_qqobqe.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230968/PlacementProject/image36_djfymy.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230969/PlacementProject/image37_zxlnue.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230969/PlacementProject/image38_u2d0da.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230969/PlacementProject/image39_u4gwkb.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230969/PlacementProject/image40_mwqich.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230969/PlacementProject/image41_fkiilh.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230970/PlacementProject/image42_mx4h0h.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230970/PlacementProject/image43_ic28uz.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230970/PlacementProject/image44_icvcaf.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230970/PlacementProject/image45_nfyk3a.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230970/PlacementProject/image46_h2c0ve.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230970/PlacementProject/image47_asm6lp.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230971/PlacementProject/image48_azf1rc.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230971/PlacementProject/image49_boikne.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230971/PlacementProject/image50_qxjvss.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230971/PlacementProject/image51_vevf8g.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230971/PlacementProject/image52_sfhatg.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230971/PlacementProject/image53_cnbveu.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230972/PlacementProject/image54_rle4gp.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230971/PlacementProject/image55_zysk6n.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230972/PlacementProject/image56_oma8ar.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230972/PlacementProject/image57_ptd3ny.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230972/PlacementProject/image58_gvljdp.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230972/PlacementProject/image59_lmvx1p.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230973/PlacementProject/image60_zebju3.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230973/PlacementProject/image61_buoq0y.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230972/PlacementProject/image62_mceldw.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230973/PlacementProject/image63_ewsttg.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230973/PlacementProject/image64_w0o2ud.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230973/PlacementProject/image65_lak9ps.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230973/PlacementProject/image66_wy6yfy.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230973/PlacementProject/image67_v3xqyw.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230973/PlacementProject/image68_h6j8zn.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230973/PlacementProject/image69_eva9ee.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230974/PlacementProject/image70_adf4yp.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230974/PlacementProject/image71_h9pyup.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230974/PlacementProject/image72_eqgew5.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230974/PlacementProject/image73_gzzcue.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230974/PlacementProject/image74_b5x7z4.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230974/PlacementProject/image75_mepetv.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230974/PlacementProject/image76_qzep28.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230974/PlacementProject/image77_jbo5o2.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230975/PlacementProject/image78_bhkhxy.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230975/PlacementProject/image79_n9ctoq.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230975/PlacementProject/image80_sxmqex.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230975/PlacementProject/image81_hfsvhj.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230975/PlacementProject/image82_hdxfbl.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230975/PlacementProject/image83_iqrfkv.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230975/PlacementProject/image84_cnyctn.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230975/PlacementProject/image85_hb2fuj.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230976/PlacementProject/image86_emxmir.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230976/PlacementProject/image87_xzx85p.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230976/PlacementProject/image88_dn8cwo.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230976/PlacementProject/image89_egh18i.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230976/PlacementProject/image90_zuuzfa.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230977/PlacementProject/image91_dvv58r.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230977/PlacementProject/image92_rpv2vn.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230977/PlacementProject/image93_thxlku.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230977/PlacementProject/image94_axxcgf.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230977/PlacementProject/image95_ayxnwq.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230977/PlacementProject/image96_xp94lf.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230977/PlacementProject/image97_hcpk8t.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230977/PlacementProject/image98_rrm9bb.jpg",
+    "https://res.cloudinary.com/dipkglaib/image/upload/v1686230977/PlacementProject/image99_tdirbb.jpg",
+  ];
+
   return (
     <div>
       <div className="section1 animated fadeIn">
@@ -37,8 +140,11 @@ function Home() {
         <h3>Our Partners Companies</h3>
         <br></br>
         <br></br>
-        <div className="section7 animated fadeIn">
-          {/* <div className="company-grid">{imageCards}</div> */}
+        <div className="company-grid">
+          {/* Generate image elements dynamically */}
+          {imageUrls.map((imageUrl, index) => (
+            <img key={index} src={imageUrl} alt={`Company ${index}`} />
+          ))}
         </div>
       </div>
 
